b59150ad58b172af5a438864a468d71b00ccc1e3
wisechengyi <wisechengyi@gmail.com>
2015-11-01 23:57:49 -0800

    incognito enabled

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 chrome.storage.local.get('headline', function (data) {
     var headline = data.headline;
     var target_url = "https://www.google.com/search?q=" + encodeURIComponent(headline);
-    chrome.windows.create({"url": target_url});
+    chrome.windows.create({"url": target_url, "incognito": true});
 })
